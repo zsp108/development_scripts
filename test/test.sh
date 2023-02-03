bd495081a7252a959f98ee3fac6f431b3acd19a9
zsp108 <echo996@foxmail.com>
2023-02-04 01:06:05 +0800

    add test

--- test/test.sh
+++ test/test.sh
@@ -7,9 +7,9 @@ SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 ls
 echo "$SCRIPT_ROOT/installs"
 cd $SCRIPT_ROOT/installs
-./env_setting.sh < $SCRIPT_ROOT/test/env_setting.data
-cat ~/.bashrc
-./git.sh < $SCRIPT_ROOT/test/git_test.data
-source ~/.bashrc && git config --global --list
-# ./go.sh
+# ./env_setting.sh < $SCRIPT_ROOT/test/env_setting.data
+# cat ~/.bashrc
+# ./git.sh < $SCRIPT_ROOT/test/git_test.data
+# source ~/.bashrc && git config --global --list
+./go.sh < $SCRIPT_ROOT/test/go_test.data
 ./python.sh < $SCRIPT_ROOT/test/python_test.data
